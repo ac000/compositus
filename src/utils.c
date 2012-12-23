8ba8eeb0b7373bee34c6c22e2a92a23477e07c1d
Andrew Clayton <andrew@opentechlabs.co.uk>
2012-12-23 12:44:29 +0000

    utils: Fix the comment for generate_hash()
    Since commit f0d4328 'utils: Allow generate_hash() to create SHA1
    hashes' added the ability to create SHA1 hashes as well as SHA256
    hashes, fix the comment for the function to reflect this.
    
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

--- src/utils.c
+++ src/utils.c
@@ -140,9 +140,11 @@ char *username_to_name(const char *username)
 }
 
 /*
- * Generates a hash (currently SHA-256). using /dev/urandom as a
- * source of entropy. The type argument should currently be passed
- * in as SHA256, though it's currently unused.
+ * Generates a hash of the specified type, using /dev/urandom as a
+ * source of entropy.
+ *
+ * It plaves the resultant hash in hash and also returns a pointer
+ * to it.
  */
 char *generate_hash(char *hash, int type)
 {
